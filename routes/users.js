b280f5f3e8132ce9322462743bc86b2f8ae60936
JoaoVictorCosta07 <joao.reis@alunos.ufersa.edu.br>
2026-04-08 20:55:33 -0300

    feat: adiciona rota para deletar usuário por id

--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,10 @@ router.get('/', async (req, res) => {
     try{
         const users = await prisma.user.findMany()
 
-        res.status(200).json(users)
+        return res.status(200).json(users)
     } catch(err){
         console.log(err)
-        res.status(500).json({message:'Erro no servidor, tente novamente'})
+        return res.status(500).json({message:'Erro no servidor, tente novamente'})
     }
 })
 
@@ -27,9 +27,32 @@ router.get('/:id', async (req, res) => {
             return res.status(404).json({message: "Usuário não encontrado"})
         }
 
-        res.status(200).json(user)
+        return res.status(200).json(user)
         
     } catch(err){
-        res.status(500).json({message:'Erro no servidor, tente novamente'})
+        return res.status(500).json({message:'Erro no servidor, tente novamente'})
     }
 })
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+
+        const deletedUser = await prisma.user.delete({
+            where: {
+                id: id
+            }
+        })
+
+        if (!deletedUser){
+            res.status(404).json({message: "Usuário não encontrado"})
+        }
+
+        console.log (deletedUser)
+        return res.status(201).json({message: "Usuário deletado com sucesso!"})
+    } catch(err){
+        return res.status(500).json({message: "Erro no servidor, tente novamente"})
+    }
+})
+
+export default router
