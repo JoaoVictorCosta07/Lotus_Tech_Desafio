dab9cbd1636ad605c9d4d3b4ee20bb4e3d1a715b
JoaoVictorCosta07 <joao.reis@alunos.ufersa.edu.br>
2026-04-07 21:23:33 -0300

    - feat: adiciona encriptação de senha

--- routes/public.js
+++ routes/public.js
@@ -10,18 +10,21 @@ router.post('/register', async (req, res) => {
     try{
         const user = req.body
 
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(user.password,salt)
+
         const userDB = await prisma.user.create({
             data: {
                 name: user.name,
                 email: user.email,
-                password: user.password
+                password: hashPassword
             }
         })
 
         res.status(201).json(userDB)
     } catch(err){
         console.log(err)
-        res.status(500).json({message:'Tente novamente'})
+        res.status(500).json({message:'Erro no servidor, tente novamente'})
     }
 })
 
