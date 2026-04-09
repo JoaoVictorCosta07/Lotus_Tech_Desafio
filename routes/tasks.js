f47c9c7234c6db64141dd98e01122221cf9f9d14
JoaoVictorCosta07 <joao.reis@alunos.ufersa.edu.br>
2026-04-09 18:02:46 -0300

    feat: adiciona rota para crair tasks vinculadas a projeto com logica de autenticação

---
+++ routes/tasks.js
@@ -0,0 +1,47 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient
+const router = express.Router()
+
+router.post('/', async (req, res) => {
+    try{
+        const task = req.body
+
+        if (!task){
+            return res.status(400).json({message:'Você não enviou uma tarefa'})
+        }
+        const foundProject = await prisma.project.findUnique({
+            where:{
+                id: task.project_id
+            }
+        })
+        
+        if (!foundProject){
+            return res.status(404).json({message:'Projeto não encontrado'})
+        }
+
+        if (foundProject.user_id != req.user.id && foundProject.shared == false){
+            return res.status(403).json({message:'Você não tem permissões de criar tarefas para esse projeto'})
+        }
+
+        const newTask = await prisma.task.create({
+            data: {
+                project_id: task.project_id,
+                title: task.title,
+                description: task.description,
+                status: task.status,
+                priority: task.priority,
+                due_date: task.due_date
+            }
+        })
+
+        return res.status(201).json(newTask)
+        
+    } catch(err){
+        console.log(err)
+        return res.status(500).json({message:'Erro no servidor, tente novamente'})
+    }
+})
+
+export default router
--- server.js
+++ server.js
@@ -2,15 +2,17 @@ import express from 'express'
 import authRoutes from './routes/auth.js'
 import usersRoutes from './routes/users.js'
 import projectsRoutes from './routes/projects.js'
+import taskRoutes from './routes/tasks.js'
 
 import auth from './middlewares/auth.js'
 
-const PORT = 3000
+const PORT = process.env.PORT
 const app = express()
 app.use(express.json())
 
 app.use('/auth', authRoutes)
 app.use('/users', auth, usersRoutes)
 app.use('/projects', auth, projectsRoutes)
+app.use('/tasks', auth, taskRoutes)
 
 app.listen(PORT, () => console.log(`servidor rodando na porta ${PORT}`))
